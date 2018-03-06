e33e61676e6bcd904a65821b061361353d884f3d
manuelblanch <manuelblanch@iesebre.com>
2018-03-06 17:10:20 +0100

    todo

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -9,5 +9,31 @@ use Faker\Generator;
 
 class CrudController extends Controller
 {
-  // Methods
+
+  public function create(Generator $faker)
+{
+  $crud = new Crud();
+  $crud->name = $faker->lexify('????????');
+  $crud->color = $faker->boolean ? 'red' : 'green';
+  $crud->save();
+
+  return response($crud->jsonSerialize(), Response::HTTP_CREATED);
+}
+
+public function index()
+{
+  return response(Crud::all()->jsonSerialize(), Response::HTTP_OK);
+}
+
+public function update(Request $request, $id)
+{
+  $crud = Crud::findOrFail($id);
+  $crud->color = $request->color;
+  $crud->save();
+
+  return response(null, Response::HTTP_OK);
+}
+
+
+
 }

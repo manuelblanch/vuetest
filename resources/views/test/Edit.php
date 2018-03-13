24b63185f6136d8070449b454e3c4a7face075ac
manuelblanch <manuelblanch@iesebre.com>
2018-03-13 17:30:09 +0100

    todo

--- resources/views/test/Edit.php
+++ resources/views/test/Edit.php
@@ -18,7 +18,6 @@
                             <label for="name" class="col-md-4 control-label">Nom</label>
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ $brand->name }}" required autofocus>
-
                                 @if ($errors->has('name'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>

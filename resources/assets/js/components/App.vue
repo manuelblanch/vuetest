7a5fd3627160e9af1814349a4b4ccff110ac470e
manuelblanch <manuelblanch@iesebre.com>
2018-04-10 16:46:05 +0200

    todo

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -15,3 +15,38 @@
     </div>
   </div>
 </template>
+
+<script>
+  function Crud({ id, color, name}) {
+    this.id = id;
+    this.color = color;
+    this.name = name;
+  }
+
+  import CrudComponent from './CrudComponent.vue';
+
+  export default {
+    data() {
+      return {
+        cruds: []
+      }
+    },
+    methods: {
+      create() {
+
+      },
+      read() {
+
+      },
+      update(id, color) {
+
+      },
+      del(id) {
+        
+      }
+    },
+    components: {
+      CrudComponent
+    }
+  }
+</script>

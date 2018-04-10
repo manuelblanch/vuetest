78cf4b518cc45940c7c656c5cdee9201ad462139
manuelblanch <manuelblanch@iesebre.com>
2018-04-10 16:33:44 +0200

    todo

--- resources/assets/js/components/CrudComponent.vue
+++ resources/assets/js/components/CrudComponent.vue
@@ -17,3 +17,31 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+   computed: {
+     image() {
+       return `/images/${this.color}.png`;
+     }
+   },
+   methods: {
+     update(val) {
+       this.$emit('update', this.id, val.target.selectedOptions[0].value);
+     },
+     del() {
+       this.$emit('delete', this.id);
+     }
+   },
+   props: ['id', 'color', 'name'],
+   filters: {
+     properCase(string) {
+       return string.charAt(0).toUpperCase() + string.slice(1);
+     }
+   }
+ }
+</script>
+
+<style>
+
+</style>

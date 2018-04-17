624ab2b3838fcfe65fe68697ac9ea531fd2bd7d3
manuelblanch <manuelblanch@iesebre.com>
2018-04-17 16:33:14 +0200

    todo

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -11,3 +11,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Crud</title>
+  <style>
+    html, body {
+      margin: 0;
+      padding: 0;,
+      height: 100%;
+      width: 100%;
+      background-colour: #01A9DB
+    }
